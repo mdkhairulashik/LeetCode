59d37da2961ae0f64794091b2d0ec1d361fd8360
Md. Khairul Ashik <khairulashik124@gmail.com>
2026-01-29 09:00:17 -0600

    Time: 16 ms (73%), Space: 20.2 MB (37.8%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest_right = 0
+        for i,x in enumerate(nums):
+            if i > farthest_right:
+                return False
+            farthest_right = max (farthest_right, i+x)
+        return True
+
+        
